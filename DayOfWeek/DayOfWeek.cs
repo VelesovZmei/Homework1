ab03593a0c036bace89083ac897801ab077e5bf8
Zmei <gar0tarkhan>
2020-04-05 20:21:06 +0300

    First

---
+++ DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework1
+{
+    class GetDayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What the day?");
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "1":
+                    Console.WriteLine("Monday");
+                    break;
+                case "2":
+                    Console.WriteLine("Tuesday");
+                    break;
+                case "3":
+                    Console.WriteLine("Wednesday");
+                    break;
+                case "4":
+                    Console.WriteLine("Thursday");
+                    break;
+                case "5":
+                    Console.WriteLine("Friday");
+                    break;
+                case "6":
+                    Console.WriteLine("Saturday");
+                    break;
+                case "7":
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+        }
+    }
+}
---
+++ MultiplicationTable/Multiple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int b = 0; b <= 9; b++)
+            {
+                Console.WriteLine($"{a} x {b} = {a * b}");
+            }
+        }
+    }
+}
